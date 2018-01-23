a875b8316b2b8cf4eb73e66a682ef9ea5c504dd5
JimenaZubaran <jimenab.zubaran@gmail.com>
2018-01-23 08:27:20 -0600

    primera version

---
+++ js/app.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    setTimeout(function() {
+        $(".splash").fadeOut(1000);
+    },1000);
+
+  setTimeout(function() {
+      $(".food-content").fadeIn(2000);
+    }, 2000);
+});
---
+++ js/data.js
@@ -0,0 +1,30 @@
+var narvarte = [
+  {
+    "name": "El Rey de las ahogadas";
+    "open-now" : true,
+    "kind-food": "mexicana";
+    "price" : 80,
+    "photo": "../assets/images/ahogadas.jpg"
+  },
+  {
+    "name": "Pescadito";
+    "open-now" : true,
+    "kind-food": "mariscos";
+    "price" : 90,
+    "photo": "../assets/images/pescadito.png"
+  },
+  {
+    "name": "Piloncillo y cascabel";
+    "open-now" : true,
+    "kind-food": "mexicana";
+    "price" : 150,
+    "photo": "../assets/images/piloncillo.jpg"
+  },
+{
+  "name": "Pinche Gringo";
+  "open-now" : true,
+  "kind-food": "carne";
+  "price" : 200,
+  "photo": "../assets/images/pinche-gringo.jpg"
+},
+];
